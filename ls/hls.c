d7184366a8b9ebc093a626862ad12816d297c00b
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-12-03 17:33:45 -0400

    holberton ls

---
+++ ls/hls.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+int main(void){
+    DIR *dir = opendir(".");
+    struct dirent *read;
+
+    while ((read = readdir(dir))){
+        // Print file name
+        printf("%s\n", read->d_name);
+    }
+
+    return 0;
+}
