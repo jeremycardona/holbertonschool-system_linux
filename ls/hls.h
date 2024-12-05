b627a1f4c8f4198f1f7fcd6245ecaa91f472ca05
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-12-05 13:05:52 -0400

    compile program

--- ls/hls.h
+++ ls/hls.h
@@ -1,8 +1,11 @@
 #ifndef HLS_H
 #define HLS_H
-#include <stddef.h>  
 
-int read_directory(DIR *dir, char ***filenames, size_t *count, size_t *capacity)';'
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int read_directory(DIR *, char ***filenames, size_t *count, size_t *capacity);
 int resize_filenames(char ***filenames, size_t *count, size_t *capacity);
 
-#endif 
+#endif
