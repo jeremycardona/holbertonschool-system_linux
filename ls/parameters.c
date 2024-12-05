f5b3a4eaac956ac776432cf4b71bd44e7b3d7ee0
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-12-05 14:10:34 -0400

    fix betty style

--- ls/parameters.c
+++ ls/parameters.c
@@ -1,56 +1,54 @@
+#include <sys/stat.h>
 #include <stdio.h>
-#include <errno.h>
-#include <stddef.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include "utils.h"
-#include "hls.h"
+#include <errno.h>
+#include <string.h>
 
 /**
- * strerror_custom - Custom function to convert errno to a string message.
- * @errnum: The error number.
- * Return: A string with the corresponding error message.
+ * strerror_custom - custom strerror function to handle errors.
+ * @errnum: the error number.
+ *
+ * Return: a string corresponding to the error.
  */
 const char *strerror_custom(int errnum)
 {
 	switch (errnum)
 	{
-	case EACCES:
-		return ("Permission denied");
-	case ENOENT:
-		return ("No such file or directory");
-	case ENOTDIR:
-		return ("Not a directory");
-	case EISDIR:
-		return ("Is a directory");
-	case ENOMEM:
-		return ("Out of memory");
-	case EEXIST:
-		return ("File exists");
-	case ENAMETOOLONG:
-		return ("File name too long");
-	default:
-		return ("Unknown error");
+		case EACCES:
+			return ("Permission denied");
+		case ENOENT:
+			return ("No such file or directory");
+		case ENOTDIR:
+			return ("Not a directory");
+		case EISDIR:
+			return ("Is a directory");
+		case ENOMEM:
+			return ("Out of memory");
+		case EEXIST:
+			return ("File exists");
+		case ENAMETOOLONG:
+			return ("File name too long");
+		default:
+			return ("Unknown error");
 	}
 }
 
 /**
- * print_error - Prints error message when directory cannot be accessed.
- * @prog_name: The program name.
- * @dir: The directory/file name.
+ * print_error - prints an error message.
+ * @prog_name: name of the program.
+ * @dir: the directory or file causing the error.
  */
 void print_error(const char *prog_name, const char *dir)
 {
-	/* Ensure both prog_name and dir are strings */
-	fprintf(stderr, "%s: cannot access '%s': %s\n",
-		prog_name, dir, strerror_custom(errno));
+	fprintf(stderr, "%s: cannot access '%s': %s\n", prog_name, dir,
+			strerror_custom(errno));
 }
 
 /**
- * print_directory_contents - Sort and print the contents of a directory.
- * @dir_name: The name of the directory.
- * @filenames: List of filenames in the directory.
- * @count: The number of filenames.
+ * print_directory_contents - prints the directory contents.
+ * @filenames: array of filenames.
+ * @count: number of filenames.
  */
 void print_directory_contents(char **filenames, size_t count)
 {
@@ -63,45 +61,60 @@ void print_directory_contents(char **filenames, size_t count)
 }
 
 /**
- * process_directory - Process and list files in a directory.
- * @dir_name: The name of the directory to process.
- * Return: 0 on success, -1 on failure.
+ * process_directory - processes a directory and lists its contents.
+ * @dir_name: the directory name.
+ *
+ * Return: 0 if success, -1 if error.
  */
 int process_directory(const char *dir_name)
 {
-	DIR *dir = opendir(dir_name);
+	struct stat statbuf;
 
-	if (!dir)
+	if (stat(dir_name, &statbuf) == -1)
 	{
-		print_error(dir_name, dir_name);
+		print_error("hls_01", dir_name);
 		return (-1);
 	}
 
-	char **filenames = malloc(sizeof(char *) * 10);
-	size_t count = 0, capacity = 10;
-
-	if (read_directory(dir, &filenames, &count, &capacity) == -1)
+	if (S_ISDIR(statbuf.st_mode))
 	{
+		DIR *dir = opendir(dir_name);
+
+		if (!dir)
+		{
+			print_error("hls_01", dir_name);
+			return (-1);
+		}
+
+		char **filenames = malloc(sizeof(char *) * 10);
+		size_t count = 0, capacity = 10;
+
+		if (read_directory(dir, &filenames, &count, &capacity) == -1)
+		{
+			closedir(dir);
+			free(filenames);
+			return (-1);
+		}
+
 		closedir(dir);
-		free(filenames);
-		return (-1);
+		sort_filenames(filenames, count);
+		print_directory_contents(filenames, count);
 	}
-
-	closedir(dir);
-
-	sort_filenames(filenames, count);
-	print_directory_contents(filenames, count);
+	else
+	{
+		/* It's not a directory, it's a file */
+		printf("%s\n", dir_name);
+	}
 
 	return (0);
 }
 
 /**
- * process_arguments - Process each directory or file passed as an argument.
- * @argc: Argument count.
- * @argv: Argument vector.
+ * process_arguments - processes the command line arguments.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
  *
- * Description: Process and list files in the directories/files.
- * Return: Always 0.
+ * Return: 0 if success, EXIT_FAILURE if no directories found.
  */
 int process_arguments(int argc, char *argv[])
 {
